3f30bd868de212630c9604ea359cf1051e3e77f9
Rishav-mngo <rishavinmngo@gmail.com>
2022-07-31 22:49:52 +0530

    Fixing bug while expansion and limit the scope of expansion.

--- README.md
+++ README.md
@@ -5,23 +5,27 @@
 
 <a id="org2704bb3"></a>
 
-# Web Mode And Rjsx Mode
+# Problem
+
+## Web Mode Vs Rjsx Mode
 
 I recently switched to emacs and instanly impressed by the extensiblity and on the other hand
 overwhelmed by the options emacs has to offer. I mostly code in javscript and recently started
 React js. Coming straight to the point emacs has a feature called mode for every type of file.
-like org-mode, prog-mode, js-mode etc. So I have to choose between web mode and rjsx mode for
-jsx file. Rjsx mode is not very good with js file as i experienced but one thing i like about
-rjsx mode is the auto paring expansion which is different from emmet mode or web mode. So I wrote
-a auto pairing like rjsx mode for web mode myself.
+Like org-mode, prog-mode, js-mode etc. So I have to choose between web mode and rjsx mode for
+editing jsx file. Rjsx mode is not very good with js file as i experienced but one thing i like 
+about rjsx mode is the auto paring expansion which is different from emmet mode or web mode. 
+So I wrote a auto pairing like rjsx mode for web mode myself.
 
 
-> I make this util to work with only web mode. I didn't find a way to map the key bindings for web mode.
+> Mode can be changed manually. I didn't find a way to map the key bindings for web mode.
 
 <img src="./demo.gif"/>
 
 
-Improvements:
+Featuers:
 
-1. Some way to indetify when to expand. Like not to expand in `if` else or `for` loops
+1. Easy expansion doesn't break your flow.
+2. Smart enough so it will not expand in unexptected area like less than or greater than expression.
+
 
--- util.el
+++ util.el
@@ -1,34 +1,72 @@
+(setq mode 'web-mode)
+(defun checkScope (curPos)
+  (setq bufStr (buffer-substring-no-properties (line-beginning-position) (line-end-position)))
+  (setq bufLis "")
+  (setq bool nil)
+  (setq bufStr (string-trim (substring bufStr 0 (- curPos 1))))
+  (if (> (length bufStr) 0)
+      (
+       progn
+       (setq bufLis (car (last (butlast (split-string (car (last (split-string bufStr))) "")))))
+       (if(string-match "^[a-zA-Z0-9)]*$" bufLis)
+	   (setq bool nil)
+	 (setq bool t)
+	 )
+       )
+    (setq bool t)
+    )
+  bool
+  )
+
 (defun getLine ()
   (interactive)
-  (if(derived-mode-p 'web-mode)
-      (progn
-	(setq curPos (point))
-	(insert "</>")
-	(goto-char (+ curPos 1))
-	(message  (buffer-substring-no-properties (line-beginning-position) (line-end-position))))
-    (insert "<")))
-
+  (if(and  (checkScope (+ 1 (current-column))) (derived-mode-p mode))
+      (
+       progn
+       (setq curPos (point))
+       (insert "</>")
+       (goto-char (+ curPos 1))
+       )
+    (insert "<")
+    )
+  )
 
 
 (defun getLine2 ()
   (interactive)
-  (if(derived-mode-p 'web-mode)
-      (progn
-	(insert ">")
-	(setq columnNumber (- (current-column) 1))
-	(setq cursorPos (point))
-	(setq lineStr  (buffer-substring-no-properties (line-beginning-position) (line-end-position)))
-	(setq diff 9999)
-	(setq pos 0)
-	(setq lis '())
-	(dotimes (index (- (length lineStr) 1))
-	  (if(= 60 (aref lineStr index))
-	      (if(< (- columnNumber index) diff)
-		  (setq pos index))))
-	(setq node (split-string (substring lineStr (+ pos 1) columnNumber)))
-	(insert "<")
-	(setq currenPos (point))
-	(goto-char (+ currenPos 1))
-	(insert (car node))
-	(goto-char cursorPos))
+  (if(derived-mode-p mode)
+      (
+       progn
+       (insert ">")
+       (setq columnNumber (- (current-column) 1))
+       (setq cursorPos (point))
+       (setq lineStr  (buffer-substring-no-properties (line-beginning-position) (line-end-position)))
+       (setq diff 9999)
+       (setq pos 0)
+       (setq found nil)
+       (setq lis '())
+       (dotimes (index (- (length lineStr) 1))
+	 (if (and (= 60 (aref lineStr index)) (> columnNumber index))
+	     
+	     (if(< (- columnNumber index) diff)
+		 (
+		  progn
+		  (setq pos index)
+		  (setq diff (- columnNumber index))
+		  (setq found t)
+		  ))
+	   )
+	 )
+       (if found
+	   (
+	    progn
+	    (setq node (split-string (substring lineStr (+ pos 1) columnNumber)))
+	    (insert "<")
+	    (setq currenPos (point))
+	    (goto-char (+ currenPos 1))
+	    (insert (car node))
+	    (goto-char cursorPos)
+	    )
+	 )
+       )
     (insert ">")))
